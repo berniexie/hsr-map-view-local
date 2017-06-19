94068729ed32751eae3aa03c29c47437dd3eee90
Abigail Dodd <adodd591@email.arizona.edu>
2017-06-19 11:29:40 -0700

    Event handler that passes the id of the card the mouse hovers on

--- src/components/ResultsListComponent.js
+++ src/components/ResultsListComponent.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react'
 
 class ResultsListComponent extends Component {
+	constructor(props) {
+        super(props);
+        this.state = {
+            selectedHotel: '',
+            searchResults: {}
+        }
+        this.handleMouseOver = this.handleMouseOver.bind(this);
+    }
+	
+	handleMouseOver(event){
+		var hotelid = event.target.id;
+		this.setState(previousState => {
+			return {selectedHotel: {hotelid}};
+		});
+		this.props.updateSelectedHotel(hotelid);
+	}
+
     render() {
-        return <h1>Results List Component</h1>
+        return (
+        	<div>
+        	<h1>Results List Component</h1>
+        	<div id="1" onMouseOver={this.handleMouseOver}>Item1</div>
+        	<div id="2" onMouseOver={this.handleMouseOver}>Item2</div>
+        	<div id="3" onMouseOver={this.handleMouseOver}>Item3</div>
+        	</div>
+        	);
     }
 }
 
--- src/components/ResultsView.js
+++ src/components/ResultsView.js
@@ -3,13 +3,25 @@ import MapContainer from './maps/Container'
 import ResultsListComponent from './ResultsListComponent'
 
 class ResultsView extends Component {
+	constructor(props) {
+        super(props);
+        this.updateSelectedHotel = this.updateSelectedHotel.bind(this);
+    }
+
+	updateSelectedHotel(hotelid){
+		console.log(hotelid);
+		//Here's where we would pass it down 
+	}
+
     render() {
         return <div>
             <h1>Results View</h1>
             <MapContainer/>
-            <ResultsListComponent/>
+            <ResultsListComponent updateSelectedHotel={this.updateSelectedHotel}/>
         </div>
     }
 }
 
 export default ResultsView
+
+//Eventually will need to pass a function whenever the map pans and zooms
