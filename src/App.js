6a3d662cfef17e8832089f26d64b08946ee4b694
berniexie <bxie94@gmail.com>
2017-06-19 14:22:05 -0700

    added material-ui

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import {
     BrowserRouter as Router,
     Route
 } from 'react-router-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import SearchView from './components/SearchView'
 import ResultsView from './components/ResultsView'
+var injectTapEventPlugin = require("react-tap-event-plugin");
+injectTapEventPlugin();
 
 const App = () => (
-    <Router>
-        <div>
-            <Route exact path="/" component={SearchView}/>
-            <Route path="/results/:cityName/:checkInDate/:checkOutDate" component={ResultsView}/>
-        </div>
-    </Router>
+    <MuiThemeProvider>
+        <Router>
+            <div>
+                <Route exact path="/" component={SearchView}/>
+                <Route path="/results/:cityName/:checkInDate/:checkOutDate" component={ResultsView}/>
+            </div>
+        </Router>
+    </MuiThemeProvider>
 )
 export default App
--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
-import {withRouter} from "react-router-dom";
+import {withRouter} from 'react-router-dom';
+import RaisedButton from 'material-ui/RaisedButton';
+import DatePicker from 'material-ui/DatePicker';
+import TextField from 'material-ui/TextField';
+import Moment from 'moment';
 
 class SearchView extends Component {
     constructor() {
@@ -7,19 +11,15 @@ class SearchView extends Component {
         this.state = {
             searchCriteria: {
                 cityName: '',
-                checkInDate: '',
-                checkOutDate: ''
+                checkInDate: null,
+                checkOutDate: null
             },
             searchResults: {}
         }
         this.handleChange = this.handleChange.bind(this);
+        this.setCheckInDate = this.setCheckInDate.bind(this);
+        this.setCheckOutDate = this.setCheckOutDate.bind(this);
         this.search = this.search.bind(this);
-    }
-
-    search() {
-        const criteria = this.state.searchCriteria;
-        const resultsRoute = '/results/' + criteria.cityName + '/' + criteria.checkInDate + '/' + criteria.checkOutDate
-        this.props.history.push(resultsRoute);
     }
 
     handleChange(event) {
@@ -30,19 +30,44 @@ class SearchView extends Component {
         this.setState({
             searchCriteria: prevState
         });
-        console.log(this.state.searchCriteria);
+    }
+
+    setCheckInDate(event, date) {
+        let prevState = this.state.searchCriteria;
+        prevState['checkInDate'] = date;
+        this.setState({
+            searchCriteria: prevState
+        });
+    }
+
+    setCheckOutDate(event, date) {
+        let prevState = this.state.searchCriteria;
+        prevState['checkOutDate'] = date;
+        this.setState({
+            searchCriteria: prevState
+        });
+    }
+
+    search() {
+        const criteria = this.state.searchCriteria;
+        const resultsRoute = '/results/' + criteria.cityName + '/' + Moment(criteria.checkInDate).format('MM-DD-YYYY') + '/' + Moment(criteria.checkOutDate).format('MM-DD-YYYY')
+        this.props.history.push(resultsRoute);
     }
 
     render() {
         return (
-            <div className="search-bar">
-                <h1>Search View</h1>
-                <ul>
-                    <input type="text" name="cityName" value={this.state.searchCriteria.cityName} onChange={this.handleChange} />
-                    <input type="text" name="checkInDate" value={this.state.searchCriteria.checkInDate} onChange={this.handleChange} />
-                    <input type="text" name="checkOutDate" value={this.state.searchCriteria.checkOutDate} onChange={this.handleChange} />
-                </ul>
-                <button className="searchButton" onClick={() => this.search()}>Search</button>
+            <div className="search-container">
+                <div className="search-bar">
+                    <h1>Welcome to Better HSR</h1>
+                    <ul className="search-input">
+                        <TextField hintText="City/Region" type="text" name="cityName" value={this.state.searchCriteria.cityName} onChange={this.handleChange} />
+                        <div className="date-input">
+                            <DatePicker hintText="Check In Date" mode="landscape" name="checkInDate" value={this.state.searchCriteria.checkInDate} onChange={this.setCheckInDate} />
+                            <DatePicker hintText="Check Out Date" mode="landscape" name="checkOutDate" value={this.state.searchCriteria.checkOutDate} onChange={this.setCheckOutDate} />
+                        </div>
+                    </ul>
+                    <RaisedButton className="search-button" onClick={() => this.search()}>Search</RaisedButton>
+                </div>
             </div>
         )
     }
