24ca1e1a5ed4d72e941e88a9c6fb8c569280542b
berniexie <bxie94@gmail.com>
2017-06-20 14:17:06 -0700

    generic styling to result cards

--- src/components/ResultComponent.js
+++ src/components/ResultComponent.js
@@ -6,10 +6,18 @@ class ResultComponent extends Component {
     }
 
     render() {
+        const result = this.props.result;
+        console.log(result);
         return (
             <div className="result-component">
+                <img className="hotel-image" src={"https://images.trvl-media.com" + result.image.small} />
                 <div className="result-content">
-                    {this.props.result.propertyName}
+                    <h3>{result.propertyName}</h3>
+                    <span className="result-information">
+                        <p>price: {result.price}</p>
+                        <p>stars: {result.star}</p>
+                    </span>
+                    <a className="more-info">more info</a>
                 </div>
             </div>
         )
