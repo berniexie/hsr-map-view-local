e1f83fb30733739c81dd54b7329fde984b266627
berniexie <bxie94@gmail.com>
2017-06-19 18:24:55 -0700

    just before adding waypoints

--- src/components/maps/Container.js
+++ src/components/maps/Container.js
@@ -34,5 +34,4 @@ class Container extends Component {
 //this needs to be refactored
 export default GoogleApiComponent({
     apiKey : 'AIzaSyAvHEM53jt2i4y-VRiibELAcBVKkLMAKds'
-    //apiKey: 'AIzaSyB6A_FPXPxidc3vWP-Z5eEXddcNrti4iVM'
 })(Container)
