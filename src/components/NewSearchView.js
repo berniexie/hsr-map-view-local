7eed12a17303cf158dfc162854edde80fdae9e6f
Abigail Dodd <adodd591@email.arizona.edu>
2017-07-10 11:51:58 -0700

    added my new search page

---
+++ src/components/NewSearchView.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {withRouter} from 'react-router-dom';
+
+class MySearchView extends Component{
+	constructor(){
+		super();
+		//other stuff
+	}//end constructor
+
+	render(){
+		return (
+			<div className="new-search-container">
+				<h1> My new search landing page</h1>
+			</div>
+			)
+	}
+
+}
